a2a45ae90b444b7461a19c239f736f8e0b233cb8
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2025-04-11 07:06:55 +0000

    m3

--- neurovision.py
+++ neurovision.py
@@ -84,7 +84,7 @@ if uploaded_file:
     with col1:
         st.image(original_img_rgb, caption="📷 Original Image", use_container_width=False)
     with col2:
-        st.image(prediction_img.resize(2*(pred_h,pred_w)), caption="🎯 YOLOv8 Prediction", use_container_width=False)
+        st.image(prediction_img.resize((2*pred_h,2*pred_w)), caption="🎯 YOLOv8 Prediction", use_container_width=False)
 
 else:
     st.info("👈 Upload an MRI image to get started.")
